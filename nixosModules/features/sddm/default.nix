64992f35df234f5f48b6b6ebb42eefc7577df89a
tohmais <callum.wishart@protonmail.com>
2024-06-08 17:23:48 +1000

    sddm fix

---
+++ nixosModules/features/sddm/default.nix
@@ -0,0 +1,15 @@
+{pkgs, lib, ...}: let
+  sddmTheme = import ./sddm-theme.nix {inherit pkgs;};
+in {
+  services.displayManager.sddm = {
+    enable = true;
+    wayland.enable = true;
+    theme = "${sddmTheme}";
+  };
+
+  environment.systemPackages = with pkgs; [
+    libsForQt5.qt5.qtquickcontrols2
+    libsForQt5.qt5.qtgraphicaleffects
+    libsForQt5.qt5.qtsvg
+  ];
+}
