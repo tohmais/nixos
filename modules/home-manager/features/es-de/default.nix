3385a52f767c42bdccc843386b8152c53ed31d5c
tohmais <callum.wishart@protonmail.com>
2025-02-13 09:55:51 +1000

    feat: add a couple comments about the stupid bullshit

--- modules/home-manager/features/es-de/default.nix
+++ modules/home-manager/features/es-de/default.nix
@@ -1,6 +1,13 @@
-{config, ...}: {
+{config, ...}: let
+/*   mkMustache = inputs.tohmutils.lib.mkMustache; */
+in {
   home.file = {
-    "ES-DE/custom_systems/es_find_rules.xml".text = ''
+    "ES-DE/custom_systems/es_find_rules.xml"
+    
+    # .source = mkMustache "es_find_rules.xml" ./es_find_rules.xml.mustache config.home;
+    
+    
+     .text = ''
       <ruleList>
             <core name="RETROARCH">
                 <rule type="corepath">
@@ -10,6 +17,6 @@
                 </rule>
             </core>
         </ruleList>
-    '';
+    ''; 
   };
 }
