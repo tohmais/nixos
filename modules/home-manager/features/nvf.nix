f5c36a4fdebc778331d231ae84f021043a1ed28b
tohmais <callum.wishart@protonmail.com>
2025-06-17 10:19:20 +1000

    fix: fix nvf+stylix config

--- modules/home-manager/features/nvf.nix
+++ modules/home-manager/features/nvf.nix
@@ -59,36 +59,7 @@ in {
           whichKey.enable = true;
         };
         statusline.lualine.enable = true;
-        theme = {
-          enable = true;
-          name = "base16";
-          style = "dark";
-          base16-colors = {
-            inherit
-              (colors)
-              base00
-              base01
-              base02
-              base03
-              base04
-              base05
-              base06
-              base07
-              ;
-            inherit
-              (colors)
-              base08
-              base09
-              base0A
-              base0B
-              base0C
-              base0D
-              base0E
-              base0F
-              ;
-          };
-          transparent = false;
-        };
+        
       };
     };
   };
