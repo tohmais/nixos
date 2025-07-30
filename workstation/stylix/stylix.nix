787264ddad0aa128999ad1a35e5ea3e4f382ddb6
tohmais <callum.wishart@protonmail.com>
2025-07-30 13:59:42 +1000

    wip: add stylix, remove ifd violation

---
+++ workstation/stylix/stylix.nix
@@ -0,0 +1,55 @@
+{
+  pkgs,
+  inputs,
+  config,
+  mainUser,
+  userPkgs,
+  ...
+}: {
+  imports = [inputs.stylix.homeModules.stylix];
+  
+  hm.stylix = {
+    enable = true;
+    base16Scheme = "./kanagawa.yaml";
+
+    polarity = "dark";
+    image = ../../assets/wallhaven-y8e1vl.jpg;
+
+    fonts = {
+      monospace = {
+        package = pkgs.nerd-fonts.lilex;
+        name = "Lilex Nerd Font Mono";
+      };
+      emoji = {
+        package = pkgs.twitter-color-emoji;
+        name = "Twitter Color Emoji";
+      };
+      sizes = {
+        popups = 12;
+      };
+    };
+
+    targets = {
+      firefox.profileNames = [mainUser];
+      emacs.enable = false;
+      spicetify.enable = true;
+      qt.enable = true;
+      hyprland.enable = false;
+      hyprpaper.enable = false;
+      nixcord.enable = true;
+    };
+
+    cursor = {
+      package = pkgs.capitaine-cursors;
+      name = "capitaine-cursors";
+      size = 32;
+    };
+
+    iconTheme = {
+      enable = true;
+      package = pkgs.callPackage userPkgs.catppuccin-se-icons {};
+      dark = "Catppuccin-SE";
+    };
+  };
+  environment.systemPackages = [config.hm.stylix.cursor.package];
+}
