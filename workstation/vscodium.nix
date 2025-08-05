3ae6abc37474acc80082482ee636405bc08757c8
tohmais <callum.wishart@protonmail.com>
2025-08-05 10:29:38 +1000

    wip: add extra modules

---
+++ workstation/vscodium.nix
@@ -0,0 +1,47 @@
+{
+  pkgs,
+  config,
+  osConfig,
+  ...
+}: {
+  hm = {
+    programs.vscode = {
+      enable = true;
+      package = pkgs.vscodium;
+      profiles.default = {
+        extensions = with pkgs; [
+          vscode-extensions.jnoortheen.nix-ide
+          vscode-extensions.asvetliakov.vscode-neovim
+          vscode-extensions.biomejs.biome
+          vscode-extensions.mkhl.direnv
+        ];
+        userSettings = {
+          "editor.fontLigatures" = true;
+
+          "extensions.experimental.affinity"."asvetliakov.vscode-neovim" = 1;
+          "editor.lineNumbers" = "relative";
+          "vscode-neovim.neovimClean" = true;
+
+          "nix.enableLanguageServer" = true;
+          "nix.serverPath" = "nixd";
+          "nix.serverSettings".nixd = {
+            formatting.command = ["alejandra" "--quiet" "--"];
+
+            options = {
+              nixos.expr = "(builtins.getFlake \"${config.home.homeDirectory}/nixos\").nixosConfigurations.${osConfig.networking.hostName}.options";
+              home-manager.expr = "(builtins.getFlake \"${config.home.homeDirectory}/nixos\").homeConfigurations.\"${config.home.username}@${osConfig.networking.hostName}\".options";
+            };
+          };
+
+          "workbench.sideBar.location" = "right";
+          "[typescript]"."editor.defaultFormatter" = "biomejs.biome";
+        };
+      };
+    };
+
+    home.packages = with pkgs; [
+      biome
+      code-cursor
+    ];
+  };
+}
