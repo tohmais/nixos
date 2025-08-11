7d1c92c8a0cf702e586654344900a759b75429f6
tohmais <callum.wishart@protonmail.com>
2025-08-12 09:16:44 +1000

    wip: add terminal theme and mpv

--- workstation/gui.nix
+++ workstation/gui.nix
@@ -10,6 +10,15 @@
     ];
   };
 
+  programs.mpv = {
+    enable = true;
+    config = {
+      # NOTE: if you want mpv discord screensharing, you NEED pulseaudio!
+      # i'd recommend making a entry in thunar to switch between the two.
+      hwdec = "auto";
+    };
+  };
+
   fonts.packages = with pkgs; [
     corefonts
     dejavu_fonts
