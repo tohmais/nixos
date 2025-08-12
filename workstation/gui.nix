5fe525b50c60b711d920aeda304956531b343242
tohmais <callum.wishart@protonmail.com>
2025-08-13 09:56:47 +1000

    wip: ncspot fix

--- workstation/gui.nix
+++ workstation/gui.nix
@@ -8,14 +8,14 @@
       libreoffice
       electron-mail
     ];
-  };
 
-  programs.mpv = {
-    enable = true;
-    config = {
-      # NOTE: if you want mpv discord screensharing, you NEED pulseaudio!
-      # i'd recommend making a entry in thunar to switch between the two.
-      hwdec = "auto";
+    programs.mpv = {
+      enable = true;
+      config = {
+        # NOTE: if you want mpv discord screensharing, you NEED pulseaudio!
+        # i'd recommend making a entry in thunar to switch between the two.
+        hwdec = "auto";
+      };
     };
   };
 
