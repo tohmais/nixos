fe27b3c35c116013cea25f88aeb4bb23cf21d596
tohmais <callum.wishart@protonmail.com>
2025-09-02 14:32:18 +1000

    wip: fix minor update issues, clean gcroots (saved like 100gb)

--- base/nh.nix
+++ base/nh.nix
@@ -6,7 +6,7 @@
   programs.nh = {
     enable = true;
     clean.enable = true;
-    clean.extraArgs = "--keep-since 4d --keep 3 --nogcroots";
+    clean.extraArgs = "--keep-since 4d --keep 3";
   };
 
   hm.home.sessionVariables = {
